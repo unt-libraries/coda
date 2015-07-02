61fa32fc65ea4dbc48f881efd70c82955d8bb15e
Damon Kelley <damon.kelley@gmail.com>
2015-07-02 14:50:28 -0500

    Change double quotes to single.

--- coda/coda_project/settings/test.py
+++ coda/coda_project/settings/test.py
@@ -6,8 +6,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'coda_local',
-        'USER': os.getenv('DB_MYSQL_USER', default="root"),
-        'PASSWORD': os.getenv('DB_PASSWORD', default="root"),
+        'USER': os.getenv('DB_MYSQL_USER', default='root'),
+        'PASSWORD': os.getenv('DB_PASSWORD', default='root'),
         'HOST': os.getenv('DB_HOST', default='db'),
     }
 }
