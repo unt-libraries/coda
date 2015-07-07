b88d2c3061a65080e468214f6ffad90e41e97f96
Damon Kelley <damon.kelley@gmail.com>
2015-07-07 11:38:17 -0500

    Placeholder for utils tests.

---
+++ coda/coda_mdstore/tests/test_utils.py
@@ -0,0 +1,15 @@
+import pytest
+
+
+class Test_prepare_graph_date_range:
+
+    @pytest.mark.xfail
+    def test_smoke():
+        assert 0
+
+
+class Test_calc_total_by_month:
+
+    @pytest.mark.xfail
+    def test_smoke():
+        assert 0
