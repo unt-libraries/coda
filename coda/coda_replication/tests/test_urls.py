b19f5fa48f0e7414c718c42d4029a8c0065ca24b
Damon Kelley <damon.kelley@gmail.com>
2015-08-21 15:36:43 -0500

    Tests for replication urls.

---
+++ coda/coda_replication/tests/test_urls.py
@@ -0,0 +1,32 @@
+import pytest
+
+from django.core.urlresolvers import resolve
+
+from .. import views
+
+
+pytestmark = pytest.mark.urls('coda_replication.urls')
+
+
+def test_queue():
+    assert resolve('/APP/queue/ark:/00001/codajom1/').func == views.queue
+
+
+def test_queue_collection():
+    assert resolve('/APP/queue/').func == views.queue
+
+
+def test_queue_recent():
+    assert resolve('/queue/').func == views.queue_recent
+
+
+def test_queue_html():
+    assert resolve('/queue/ark:/00001/codajom1/').func == views.queue_html
+
+
+def test_queue_search():
+    assert resolve('/queue/search/').func == views.queue_search
+
+
+def test_queue_stats():
+    assert resolve('/queue/stats/').func == views.queue_stats
