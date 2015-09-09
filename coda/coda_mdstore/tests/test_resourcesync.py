96e0da06d5d95a7bed2e2fed5eab68aabf4a2580
Damon Kelley <damon.kelley@gmail.com>
2015-09-09 16:23:33 -0500

    Add tests for capabilitylist and changelist.

--- coda/coda_mdstore/tests/test_resourcesync.py
+++ coda/coda_mdstore/tests/test_resourcesync.py
@@ -11,7 +11,7 @@ pytestmark = [
 ]
 
 
-class TestSitemap(Sitemap):
+class FakeSitemap(Sitemap):
     lastmod = None
     protocol = 'http'
 
@@ -23,7 +23,7 @@ class TestSitemap(Sitemap):
 
 
 def test_index(rf):
-    sitemaps = {'001': TestSitemap}
+    sitemaps = {'001': FakeSitemap}
     request = rf.get('/')
     response = resourcesync.index(request, sitemaps, 'mdstore/resourceindex.xml')
 
@@ -57,10 +57,73 @@ def test_sitemap_context(rf):
 
     urlset = response.context_data['urlset']
 
-    assert all([True for item in urlset if 'priority' in item.keys()])
-    assert all([True for item in urlset if 'lastmod' in item.keys()])
-    assert all([True for item in urlset if 'changefreq' in item.keys()])
-    assert all([True for item in urlset if 'location' in item.keys()])
-    assert all([True for item in urlset if 'oxum' in item.keys()])
+    assert all(True for item in urlset if 'priority' in item.keys())
+    assert all(True for item in urlset if 'lastmod' in item.keys())
+    assert all(True for item in urlset if 'changefreq' in item.keys())
+    assert all(True for item in urlset if 'location' in item.keys())
+    assert all(True for item in urlset if 'oxum' in item.keys())
 
     assert 'MOST_RECENT_BAGGING_DATE' in response.context_data
+
+
+def test_changelist(rf):
+    request = rf.get('/')
+    template_name = 'mdstore/changelist.xml'
+    content_type = 'application/xml'
+
+    response = resourcesync.changelist(request, resourcesync.sitemaps, None,
+                                       'mdstore/changelist.xml')
+
+    assert response.template_name == template_name
+    assert response.get('Content-Type', False) == content_type
+
+    assert 'MOST_RECENT_BAGGING_DATE' in response.context_data
+    assert 'urlset' in response.context_data
+
+
+def test_changelist_context(rf):
+    factories.FullBagFactory.create_batch(10)
+    request = rf.get('/')
+    response = resourcesync.changelist(request, resourcesync.sitemaps, None,
+                                       'mdstore/changelist.xml')
+    response.render()
+
+    urlset = response.context_data['urlset']
+
+    assert all(True for item in urlset if 'name' in item.keys())
+    assert all(True for item in urlset if 'size' in item.keys())
+    assert all(True for item in urlset if 'files' in item.keys())
+    assert all(True for item in urlset if 'bagging_date' in item.keys())
+
+    assert 'MOST_RECENT_BAGGING_DATE' in response.context_data
+
+
+def test_capabilitylist(rf):
+    request = rf.get('/')
+    template_name = 'mdstore/capabilitylist.xml'
+    content_type = 'application/xml'
+
+    response = resourcesync.capabilitylist(request, template_name, content_type)
+
+    assert response.template_name == template_name
+    assert response.get('Content-Type', False) == content_type
+
+    assert 'MOST_RECENT_BAGGING_DATE' in response.context_data
+
+
+class TestBaseSitemap:
+
+    def test_items(self):
+        factories.FullBagFactory.create_batch(10)
+        sitemap = resourcesync.BaseSitemap()
+        items = sitemap.items()
+
+        assert items.count() == 10
+        assert all(True for i in items if 'name' in i.keys())
+
+    def test_location(self):
+        obj = {'name': 'ark:/00001/coda1k'}
+        sitemap = resourcesync.BaseSitemap()
+
+        location = sitemap.location(obj)
+        assert location == '/bag/ark:/00001/coda1k'
