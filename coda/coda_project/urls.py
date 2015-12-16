27fe030d260cdba70220a5cc83a67201f0738067
Damon Kelley <damon.kelley@gmail.com>
2015-12-16 11:56:26 -0600

    Remove the media url and cleanup the settings.

--- coda/coda_project/urls.py
+++ coda/coda_project/urls.py
@@ -1,30 +1,14 @@
-from django.conf.urls import *
-from django.conf import settings
+from django.conf.urls import url, include
 from django.contrib import admin
-import coda_oaipmh
+
+
 admin.autodiscover()
 
-
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^oai/$', 'coda_oaipmh.views.index'),
     url(r'', include('premis_event_service.urls')),
     url(r'', include('coda_replication.urls')),
     url(r'', include('coda_mdstore.urls')),
     url(r'', include('coda_validate.urls')),
-)
-
-
-# For local development
-if settings.DEBUG:
-    # static files (images, css, javascript, etc.)
-    urlpatterns += patterns(
-        '',
-        (
-            r'^media/(?P<path>.*)$',
-            'django.views.static.serve',
-            {
-                'document_root': settings.MEDIA_ROOT,
-            },
-        ),
-    )
+]
