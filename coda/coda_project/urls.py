0c0f620d762306166c3fa33467a3982f58c4d69c
Damon Kelley <damon.kelley@gmail.com>
2015-12-16 18:04:29 -0600

    Namespace the included urls.

--- coda/coda_project/urls.py
+++ coda/coda_project/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^oai/$', 'coda_oaipmh.views.index'),
     url(r'', include('premis_event_service.urls')),
-    url(r'', include('coda_replication.urls')),
-    url(r'', include('coda_mdstore.urls')),
-    url(r'', include('coda_validate.urls')),
+    url(r'', include('coda_replication.urls', namespace='replication')),
+    url(r'', include('coda_mdstore.urls', namespace='mdstore')),
+    url(r'', include('coda_validate.urls', namespace='validate')),
 ]
