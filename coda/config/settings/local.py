bcf693d670456ef6f64c7c792c6674b861c8152e
Damon Kelley <damon.kelley@gmail.com>
2016-01-08 16:49:14 -0600

    Add the django-debug-toolbar to the local dev environment.

--- coda/config/settings/local.py
+++ coda/config/settings/local.py
@@ -5,6 +5,13 @@ SECRET_KEY = 'local-secret'
 
 SITE_ID = 1
 
+DEBUG_TOOLBAR_CONFIG = {
+    'SHOW_TOOLBAR_CALLBACK': lambda request: True
+}
+
+INSTALLED_APPS += ('debug_toolbar',)
+
+
 # Database settings for the Dockerized dev environment.
 # See docker-compose.yml
 DATABASES = {
