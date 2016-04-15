92e53eb2b38b954d3e56636545183de85c982ed3
Damon Kelley <damon.kelley@gmail.com>
2016-04-15 15:55:27 -0500

    Add a ValidateManager
    This is the model manager for the Validate model that supports
    table-level querying.

--- coda/coda_validate/models.py
+++ coda/coda_validate/models.py
@@ -1,7 +1,30 @@
 from django.db import models
 from datetime import datetime
 
-# Create your models here.
+
+class ValidateManager(models.Manager):
+
+    def last_verified_status_counts(self):
+        results = self._verified_counts()
+        results = self._format_count_results(results)
+        return self._set_default_status_counts(results)
+
+    def _set_default_status_counts(self, counts):
+        for _, value in Validate.VERIFIED_STATUS_CHOICES:
+            counts.setdefault(value, 0)
+        return counts
+
+    def _format_count_results(self, results):
+        return {
+            c['last_verified_status']: c['count']
+            for c in results
+        }
+
+    def _verified_counts(self):
+        return (self.values('last_verified_status')
+                    .annotate(count=models.Count('last_verified_status')))
+
+
 class Validate(models.Model):
     VERIFIED_STATUS_CHOICES = (
         ("Unverified", "Unverified"),
@@ -10,13 +33,15 @@ class Validate(models.Model):
     )
     identifier = models.CharField(max_length=255, unique=True, editable=False)
     added = models.DateTimeField(auto_now_add=True)
-    last_verified = models.DateTimeField(default=datetime(2000,01,01))
+    last_verified = models.DateTimeField(default=datetime(2000, 1, 1))
     last_verified_status = models.CharField(
         max_length=25, choices=VERIFIED_STATUS_CHOICES, default="Unverified"
     )
-    priority_change_date = models.DateTimeField(default=datetime(2000,01,01))
+    priority_change_date = models.DateTimeField(default=datetime(2000, 1, 1))
     priority = models.IntegerField(default=0)
     server = models.CharField(max_length=255)
+
+    objects = ValidateManager()
 
     def __unicode__(self):
         return '%s' % self.identifier
--- coda/coda_validate/tests/test_models.py
+++ coda/coda_validate/tests/test_models.py
@@ -1,4 +1,7 @@
+import pytest
+
 from .. import factories
+from ..models import ValidateManager, Validate
 
 
 class TestValidate:
@@ -6,3 +9,29 @@ class TestValidate:
     def test_unicode(self):
         validate = factories.ValidateFactory.build()
         assert unicode(validate) == validate.identifier
+
+
+@pytest.mark.django_db
+class TestValidateManager(object):
+
+    def test_last_verified_status_counts(self):
+        factories.ValidateFactory.create_batch(3, last_verified_status='Passed')
+        factories.ValidateFactory.create_batch(5, last_verified_status='Failed')
+        factories.ValidateFactory.create_batch(7, last_verified_status='Unverified')
+
+        manager = ValidateManager()
+        manager.model = Validate
+
+        counts = manager.last_verified_status_counts()
+        assert counts['Passed'] == 3
+        assert counts['Failed'] == 5
+        assert counts['Unverified'] == 7
+
+    def test_last_verified_status_counts_are_zero(self):
+        manager = ValidateManager()
+        manager.model = Validate
+
+        counts = manager.last_verified_status_counts()
+        assert counts['Passed'] == 0
+        assert counts['Failed'] == 0
+        assert counts['Unverified'] == 0
