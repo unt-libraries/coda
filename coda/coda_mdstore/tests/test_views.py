3bf8a414416ef0fc12e5f6bfd837d09d443701eb
madhulika95b <srisudhamadhulika.bayyavarapu@unt.edu>
2019-11-14 12:34:27 -0600

    Modify the mocked object return value

--- coda/coda_mdstore/tests/test_views.py
+++ coda/coda_mdstore/tests/test_views.py
@@ -612,8 +612,8 @@ class TestBagURLListView:
         self.getFileHandle.return_value.url = 'https://coda/testurl'
         # Mock what gets read from the manifest file.
         self.getFileHandle.return_value.readline.side_effect = [
-            b'<html coda/bag/123/manifest-md5.txt',
-            b'<html coda/bag/123/bagit.txt',
+            b'192e635b17a9c2aea6181f0f87cab05d  data/file01.txt',
+            b'18b7c500ef8bacf7b2151f83d28e7ca1  data/file02.txt',
             b'']
         bag = FullBagFactory.create()
         request = rf.get('/')
@@ -624,8 +624,8 @@ class TestBagURLListView:
 
         assert (b'https://coda/bag-info.txt\n'
                 b'https://coda/bagit.txt\n'
-                b'https://coda/coda/bag/123/bagit.txt\n'
-                b'https://coda/coda/bag/123/manifest-md5.txt') in response.content
+                b'https://coda/data/file02.txt\n'
+                b'https://coda/data/file01.txt') in response.content
         assert response.status_code == 200
 
 
