b244dfc17812396d8e3aa6336e42c2485f5326d7
gracieflores <53153376+gracieflores@users.noreply.github.com>
2023-05-04 15:39:06 -0500

    update settings to point to the only settings file

--- appstart.sh
+++ appstart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 /wait-for-mysqld.sh
 echo "Migrate..."
-python manage.py migrate --settings=coda.config.settings.local
+python manage.py migrate --settings=coda.config.settings
 echo "Start app..."
-python manage.py runserver 0.0.0.0:8787 --settings=coda.config.settings.local
+python manage.py runserver 0.0.0.0:8787 --settings=coda.config.settings
 
